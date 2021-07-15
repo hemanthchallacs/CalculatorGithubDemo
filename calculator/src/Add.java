b47667661f63203405ab37e59541904803aa286c
hemanthchallacs <hemanthchallacs@gmai.com>
2021-07-16 00:13:36 +0530

    4x_changes

--- calculator/src/Add.java
+++ calculator/src/Add.java
@@ -6,7 +6,7 @@ public class Add {
 
     public int adding(int a, int b)
     {
-        return a+b;
+        return (4*a)+(4*b);
     }
 
 }
--- calculator/src/Difference.java
+++ calculator/src/Difference.java
@@ -1,6 +1,6 @@
 public class Difference {
     public Difference(){}
     public int subtracting(int a, int b){
-        return a-b;
+        return (4*a)-(4*b);
     }
 }
